a82d12624e5b763feb7f977eb492b645edef5040
JEPEREZR <48518017+JEPEREZR@users.noreply.github.com>
2020-06-18 19:43:04 -0400

    fixes

--- src/resources/credentials/createPdf.jsx
+++ src/resources/credentials/createPdf.jsx
@@ -1,23 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import PDFViewer, { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
 import QRCode from 'qrcode.react'
 import ApiUrl from "../../provider/url"
 import { format } from 'rut.js'
-import Pdf from "react-to-pdf";
-import html2canvas from 'html2canvas';
-import {jsPDF} from 'jspdf'
+import { Document, Page } from 'react-pdf';
+
 import './styles.scss'
-// Create styles
-const styles = StyleSheet.create({
-    page: {
-        flexDirection: 'row',
-    },
-    section: {
-        margin: 10,
-        padding: 10,
-        flexGrow: 1
-    }
-});
 
 
 const urlValidacion = 'https://webviews.smartrancagua.com/validationCredential?code='
@@ -25,7 +12,12 @@ const urlValidacion = 'https://webviews.smartrancagua.com/validationCredential?c
 const CreatePdf = () => {
 
     const [datos, setDatos] = useState();
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
 
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+    }
     const users = async () => {
         try {
             const request = new Request(ApiUrl + '/userintegration-offline', {
@@ -56,93 +48,67 @@ const CreatePdf = () => {
         setDatos(data);
     }, []);
     const ref = React.createRef();
+    const options = {
+        orientation: 'landscape',
+        unit: 'in',
+        format: [4, 2]
+    };
 
-    const   printDocument=()=> {
-        const input = document.getElementById('divToPrint');
-        html2canvas(input)
-          .then((canvas) => {
-            const imgData = canvas.toDataURL('image/png');
-            const pdf = new jsPDF();
-            pdf.addImage(imgData, 'JPEG', 0, 0);
-            // pdf.output('dataurlnewwindow');
-            pdf.save("download.pdf");
-          })
-        ;
-      }
+
     return (
         <div className="App">
-            <Pdf targetRef={ref} filename="credencial funcionario.pdf">
-                {({ toPdf }) => <button onClick={toPdf}>Generate Pdf</button>}
-            </Pdf>
-            <div ref={ref} className='pdf'>
-                <Document>
-                    {datos && datos.map((item, index) => {
 
+                     {datos && datos.map((item, index) => {
+                return (
 
-                        return (
-                            <div key={index}>
-                                {console.log(index)}
-                                {index < 10 &&
-                                    <Page size="A4" style={styles.page} >
+                    <div className='letter' key={index}>
+                        <div className="officialCredential">
+                            <div className='logo'><img src='/assets/img/credential/logo.png'></img></div>
 
-                                        <View style={styles.section}>
-
-                                            <div className="officialCredential">
-                                                <div className='logo'><img src='/assets/img/credential/logo.png'></img></div>
-
-                                                <div className="fila">
-                                                    <div className="columna Credencial">
-                                                        <div className="backofficialCredential">
-                                                            <h4 className='text-center'>CREDENCIAL MUNICIPAL</h4>
-                                                            <div className="Flex dataCredential">
-                                                                <div className="divDatos">
-                                                                    <div className="TextoTarjeta">
-                                                                        Nombre:{' '}{item.name ? item.name : ''}
-                                                                    </div>
-                                                                    <div className="TextoTarjeta">
-                                                                        Rut:{' '}{item.identificationNumber ? format(item.identificationNumber) : ''}
-                                                                    </div>
-                                                                    {item.department ? (
-                                                                        <div className="TextoTarjeta">
-                                                                            Departamento:{' '}{item.department}
-                                                                        </div>)
-                                                                        : ''}
-
-                                                                </div>
-                                                            </div>
-
-                                                            <div className="divQr">
-                                                                <QRCode
-                                                                    value={urlValidacion + item.offlineToken}
-                                                                    bgColor="#FFFFFF"
-                                                                    fgColor="#000000"
-                                                                    includeMargin={true}
-                                                                    size={200}
-                                                                    level="L"
-                                                                />
-                                                            </div>
-                                                            <div className='textvalidation'>Escanee este código qr para validar esta Credencial</div>
-                                                        </div>
-                                                    </div>
+                            <div className="fila">
+                                <div className="columna Credencial">
+                                    <div className="backofficialCredential">
+                                        <h4 className='text-center'>CREDENCIAL MUNICIPAL</h4>
+                                        <div className="Flex dataCredential">
+                                            <div className="divDatos">
+                                                <div className="TextoTarjeta">
+                                                    Nombre:{' '}{item.name ? item.name : ''}
                                                 </div>
+                                                <div className="TextoTarjeta">
+                                                    Rut:{' '}{item.identificationNumber ? format(item.identificationNumber) : ''}
+                                                </div>
+                                                {item.department ? (
+                                                    <div className="TextoTarjeta">
+                                                        Departamento:{' '}{item.department}
+                                                    </div>)
+                                                    : ''}
+
                                             </div>
+                                        </div>
 
-                                        </View>
-
-                                    </Page>
-                                }
+                                        <div className="divQr">
+                                            <QRCode
+                                                value={urlValidacion + item.offlineToken}
+                                                bgColor="#FFFFFF"
+                                                fgColor="#000000"
+                                                includeMargin={true}
+                                                size={200}
+                                                level="L"
+                                            />
+                                        </div>
+                                        <div className='textvalidation'>Escanee este código qr para validar esta Credencial</div>
+                                    </div>
+                                </div>
                             </div>
-                        )
+                        </div>
+                    </div>
+                )
 
-                    }
-
-                    )}
-
-                </Document>
-            </div>
+            }
+            )
+                }
+        
         </div>
-
-
 
 
     );
