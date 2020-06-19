95002e6375ebb5c61f633b4ac9a7b2530baba135
José Abarca Saavedra <jose@smart.rancagua.cl>
2020-06-18 20:11:26 -0400

    order

--- src/resources/credentials/createPdf.jsx
+++ src/resources/credentials/createPdf.jsx
@@ -56,44 +56,44 @@ const CreatePdf = () => {
     }
 
     const credential = (user) => (<div className="officialCredential">
-                            <div className='logo'><img src='/assets/img/credential/logo.png'></img></div>
+        <div className='logo'><img src='/assets/img/credential/logo.png'></img></div>
 
-                            <div className="fila">
-                                <div className="columna Credencial">
-                                    <div className="backofficialCredential">
-                                        <h4 className='text-center'>CREDENCIAL MUNICIPAL</h4>
-                                        <div className="Flex dataCredential">
-                                            <div className="divDatos">
-                                                <div className="TextoTarjeta">
-                                                    Nombre:{' '}{user.name ? user.name : ''}
-                                                </div>
-                                                <div className="TextoTarjeta">
-                                                    Rut:{' '}{user.identificationNumber ? format(user.identificationNumber) : ''}
-                                                </div>
-                                                {user.department ? (
-                                                    <div className="TextoTarjeta">
-                                                        Departamento:{' '}{user.department}
-                                                    </div>)
-                                                    : ''}
-
-                                            </div>
-                                        </div>
-
-                                        <div className="divQr">
-                                            <QRCode
-                                                value={urlValidacion + user.offlineToken}
-                                                bgColor="#FFFFFF"
-                                                fgColor="#000000"
-                                                includeMargin={true}
-                                                size={200}
-                                                level="L"
-                                            />
-                                        </div>
-                                        <div className='textvalidation'>Escanee este código qr para validar esta Credencial</div>
-                                    </div>
-                                </div>
+        <div className="fila">
+            <div className="columna Credencial">
+                <div className="backofficialCredential">
+                    <h4 className='text-center'>CREDENCIAL MUNICIPAL</h4>
+                    <div className="Flex dataCredential">
+                        <div className="divDatos">
+                            <div className="TextoTarjeta">
+                                Nombre:{' '}{user.name ? user.name : ''}
                             </div>
-                        </div>)
+                            <div className="TextoTarjeta">
+                                Rut:{' '}{user.identificationNumber ? format(user.identificationNumber) : ''}
+                            </div>
+                            {user.department ? (
+                                <div className="TextoTarjeta">
+                                    Departamento:{' '}{user.department}
+                                </div>)
+                                : ''}
+
+                        </div>
+                    </div>
+
+                    <div className="divQr">
+                        <QRCode
+                            value={urlValidacion + user.offlineToken}
+                            bgColor="#FFFFFF"
+                            fgColor="#000000"
+                            includeMargin={true}
+                            size={200}
+                            level="L"
+                        />
+                    </div>
+                    <div className='textvalidation'>Escanee este código qr para validar esta Credencial</div>
+                </div>
+            </div>
+        </div>
+    </div>)
 
     return (
         <div className="App">
